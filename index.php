ea880ecf9f2957bd48b5b0527d8c1d5401ec1b86
barcza17 <bar.m2299@gmail.com>
2025-05-02 17:20:42 +0200

    URL átállítás ?page=... típusra – Front Controller javítása

--- index.php
+++ index.php
@@ -1,15 +1,14 @@
 <?php
 	include('./includes/config.inc.php');
-	$oldal = $_SERVER['QUERY_STRING'];
-	if ($oldal!="") {
-		if (isset($oldalak[$oldal]) && file_exists("./templates/pages/{$oldalak[$oldal]['fajl']}.tpl.php")) {
-			$keres = $oldalak[$oldal];
-		}
-		else { 
-			$keres = $hiba_oldal;
-			header("HTTP/1.0 404 Not Found");
-		}
+
+	$page = $_GET['page'] ?? '/'; 
+
+	if (isset($oldalak[$page]) && file_exists("./templates/pages/{$oldalak[$page]['fajl']}.tpl.php")) {
+		$keres = $oldalak[$page];
+	} else {
+		$keres = $hiba_oldal;
+		header("HTTP/1.0 404 Not Found");
 	}
-	else $keres = $oldalak['/'];
-	include('./templates/index.tpl.php'); 
+
+	include('./templates/index.tpl.php');
 ?>
