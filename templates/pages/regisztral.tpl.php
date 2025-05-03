e925a5d59f826dadc7c5c4028f8d3f51917aceea
barcza17 <bar.m2299@gmail.com>
2025-05-03 11:31:25 +0200

    Regisztrációs oldal reszponzív meta viewport hozzáadva – Monika

--- templates/pages/regisztral.tpl.php
+++ templates/pages/regisztral.tpl.php
@@ -1,9 +1,11 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>Regisztráció</title>
-        <meta charset="utf-8">
-    </head>
+<head>
+    <title>Regisztráció</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
     <body>
         <?php if(isset($uzenet)) { ?>
             <h1><?= $uzenet ?></h1>
@@ -12,4 +14,3 @@
             <?php } ?>
         <?php } ?>
     </body>  
-</html>
